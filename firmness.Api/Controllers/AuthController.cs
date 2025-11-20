d0a0ae0f7227f342ce7711aa6158e3fdee37f29e
Camilo Rodriguez <camiropi97@gmail.com>
2025-11-20 10:29:42 -0500

    auth controller completed

--- firmness.Api/Controllers/AuthController.cs
+++ firmness.Api/Controllers/AuthController.cs
@@ -1,9 +1,12 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using firmness.Domain.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace firmness.Api.Controllers;
-
 
 [ApiController]
 [Route("api/[controller]")]
@@ -22,10 +25,46 @@ public class AuthController : ControllerBase
         _signInManager = signInManager;
         _configuration = configuration;
     }
-    
+
     [HttpPost("login")]
-    public async Task<IActionResult> Login(string username, string password)
-    
-        
-    
+    public async Task<IActionResult> Login(string email, string password)
+    {
+        var user = await _userManager.FindByEmailAsync(email);
+
+        if (user == null)
+            return Unauthorized("Email doesn't exist");
+
+        var result = await _signInManager.CheckPasswordSignInAsync(user, password, false);
+        if (!result.Succeeded)
+            return Unauthorized("Invalid password");
+
+        var roles = await _userManager.GetRolesAsync(user);
+
+        var claims = new List<Claim>()
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Email),
+            new Claim("UserId", user.Id)
+        };
+
+        foreach (var role in roles)
+            claims.Add(new Claim(ClaimTypes.Role, role));
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtKey"]));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: _configuration["JwtIssuer"],
+            audience: null,
+            claims: claims,
+            expires: DateTime.UtcNow.AddHours(3),
+            signingCredentials: creds
+        );
+
+        return Ok(new
+        {
+            token = new JwtSecurityTokenHandler().WriteToken(token),
+            email = user.Email,
+            roles = roles
+        });
+    }
 }
