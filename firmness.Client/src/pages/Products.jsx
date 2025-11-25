6e00233c4aceb96ff0a7806249e90d36f7c2f4c3
Camotti <camiropi97@gmail.com>
2025-11-24 19:48:54 -0500

    app.jsx is completed and ready

--- firmness.Client/src/pages/Products.jsx
+++ firmness.Client/src/pages/Products.jsx
@@ -3,4 +3,19 @@ import {useEffect, useState} from "react";
 
 export default function Products() {
     const [products, setProducts] = useState([]);
+    
+    useEffect(() => {
+       api.get("/Products").then((res) => {
+           setProducts(res.data);
+       });
+    }, []);
+    
+    return (
+        <>
+        <h1>Catalog</h1>
+            {products.map(p => (
+                <div key={p.id}>{p.name}</div>
+            ))}
+        </>
+    );
 }
